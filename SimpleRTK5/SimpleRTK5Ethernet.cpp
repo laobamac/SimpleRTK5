23fa0a6144c15d315979f661191fb7ffda709e3f
laobamac <107761278+laobamac@users.noreply.github.com>
2026-01-30 00:11:28 +0800

    Update SimpleRTK5Ethernet.cpp

--- SimpleRTK5/SimpleRTK5Ethernet.cpp
+++ SimpleRTK5/SimpleRTK5Ethernet.cpp
@@ -5,6 +5,26 @@
 //  Created by laobamac on 2025/10/7.
 //
 
+/* LucyRTL8125Ethernet.cpp -- RTL8125 driver class implementation.
+*
+* Copyright (c) 2020 Laura Müller <laura-mueller@uni-duesseldorf.de>
+* All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation; either version 2 of the License, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+* more details.
+*
+* Driver for Realtek RTL8125 PCIe 2.5GB ethernet controllers.
+*
+* This driver is based on Realtek's r8125 Linux driver (9.003.04).
+*/
+
 #include "SimpleRTK5Ethernet.hpp"
 
 #pragma mark --- function prototypes ---
